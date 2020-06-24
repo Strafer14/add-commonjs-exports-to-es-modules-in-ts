a42ca252b38c0a153b9b831c5754f48e86cfed4e
Michael Ostrovsky <michael@zencity.io>
2020-06-24 15:35:24 +0300

    updating global

--- bin/global.js
+++ bin/global.js
@@ -1,5 +1,7 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
+
+const args = process.argv.splice(process.execArgv.length + 2);
 
 const { walk } = require('../index');
 
-walk(process.argv[2], () => { console.log('Done!') });
+walk(args[0], () => { console.log('Done!') });
